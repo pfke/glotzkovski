e1888a63caed7341fc556d97a82c1566d4cd2fa6
Heiko Blobner <h.blobner@avm.de>
2019-01-24 19:14:03 +0100

    sciter example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[macro_use]
+extern crate rust_embed;
+extern crate sciter;
+
+#[derive(RustEmbed)]
+#[folder = "src/assets/"]
+struct Asset;
+
+
+use sciter::Window;
+
 fn main() {
-    println!("Hello, world!");
+  let mut frame: Window = sciter::Window::new();
+
+  let index_html = Asset::get("minimal.htm").unwrap();
+
+  frame.load_html(index_html.as_ref(), None);
+  frame.run_app();
 }
