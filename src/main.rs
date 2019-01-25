eaba71a281a4aa73dece5da97984869721119c45
Heiko Blobner <h.blobner@avm.de>
2019-01-25 19:15:21 +0100

    std asset loading

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,15 @@ extern crate sciter;
 #[folder = "src/assets/"]
 struct Asset;
 
-
 use sciter::Window;
 
 fn main() {
   let mut frame: Window = sciter::Window::new();
 
   let index_html = Asset::get("minimal.htm").unwrap();
+  let index_so = include_bytes!("assets/minimal.htm");
 
-  frame.load_html(index_html.as_ref(), None);
+//  frame.load_html(index_html.as_ref(), None);
+  frame.load_html(index_so.as_ref(), None);
   frame.run_app();
 }
